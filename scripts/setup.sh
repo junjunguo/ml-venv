8adc32aeba3f38feb136ef90e93b2d1e77ef0e00
Junjun Guo <junjun.guo@sparebank1.no>
2023-03-02 13:13:06 +0100

    use python3.10, tf-macos=2.11, tf-metal=0.7.1; still need to use ImageDataGenerator

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,10 +5,10 @@ echo " ▶️  Creating Python virtual environment"
 echo ""
 
 rm -rf venv-metal/* && \
-  python3 -m venv venv-metal && \
+  python3.10 -m venv venv-metal && \
   source venv-metal/bin/activate && \
-  pip3 install pip --upgrade && \
-  pip3 install -r requirements.txt
+  pip3.10 install pip --upgrade && \
+  pip3.10 install -r requirements.txt
 
 echo ""
 echo " ✔️  Completed! Python virtual environment for ML created. ☀️"
